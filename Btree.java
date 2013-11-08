fdcd83744477e66a133d825f9ca535d0e2434a4d
aardvarkesque <masonlkinney@gmail.com>
2013-11-07 21:38:49 -0700

    Skeleton of Btree, with some of the easy stuff done.

--- Btree.java
+++ Btree.java
@@ -1,4 +1,21 @@
-
 public class Btree {
- //something
+	BTreeNode root,current,sibling;
+	
+
+	private class BTreeNode {
+		int degree,parent;
+		int[] children;
+
+	}
+
+	private class TreeObject {
+		long data;
+		int count;
+		
+		public TreeObject(long data){
+			this.data=data;
+			count=1;
+		}
+	}
+
 }
